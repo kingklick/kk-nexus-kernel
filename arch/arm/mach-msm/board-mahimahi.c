9639760e238dc83f8fcb77ad8500db19a1386b64
intersectR <rjgolo@gmail.com>
2010-06-27 16:41:43 +0800

    Add Hybrid Adaptive Voltage Scaling (HAVS) and increase frequency steps.
    The purpose of HAVS is to minimize the power used by the CPU by determining
    and setting the optimal voltage. At the same time, the maximum voltage by
    which HAVS can scale to is fixed to a specified voltage depending on the
    CPU frequency in order to prevent scaling to a higher voltage than what is
    normally used at a specified voltage. The optimal voltage is actively determined
    for each frequency and temperature. HAVS actively adjusts the CPU voltage as the
    CPU frequency and temperature changes.
    
    AVS Author: Dave Estes
    
    HAVS Modification Author: intersectRaven

--- arch/arm/mach-msm/board-mahimahi.c
+++ arch/arm/mach-msm/board-mahimahi.c
@@ -437,7 +437,7 @@ static struct regulator_init_data tps65023_data[5] = {
 	{
 		.constraints = {
 			.name = "dcdc1", /* VREG_MSMC2_1V29 */
-			.min_uV = 1000000,
+			.min_uV = 800000,
 			.max_uV = 1300000,
 			.valid_ops_mask = REGULATOR_CHANGE_VOLTAGE,
 		},
@@ -963,13 +963,42 @@ static void config_gpio_table(uint32_t *table, int len)
 	}
 }
 
+#ifdef CONFIG_QSD_SVS
+#define TPS65023_MAX_DCDC1	1300
+#else
+#define TPS65023_MAX_DCDC1	CONFIG_QSD_PMIC_DEFAULT_DCDC1
+#endif
+
+static int qsd8x50_tps65023_set_dcdc1(int mVolts)
+{
+        int rc = 0;
+#ifdef CONFIG_QSD_SVS
+	rc = tps65023_set_dcdc1_level(mVolts);
+	/* By default the TPS65023 will be initialized to 1.225V.
+	 * So we can safely switch to any frequency within this
+	 * voltage even if the device is not probed/ready.
+	 */
+	if (rc == -ENODEV && mVolts <= CONFIG_QSD_PMIC_DEFAULT_DCDC1)
+		rc = 0;
+#else
+	/* Disallow frequencies not supported in the default PMIC
+	 * output voltage.
+	 */
+	if (mVolts > CONFIG_QSD_PMIC_DEFAULT_DCDC1)
+		rc = -EFAULT;
+#endif
+	return rc;
+}
+
 static struct msm_acpu_clock_platform_data mahimahi_clock_data = {
 	.acpu_switch_time_us	= 20,
 	.max_speed_delta_khz	= 256000,
 	.vdd_switch_time_us	= 62,
 	.power_collapse_khz	= 245000,
 	.wait_for_irq_khz	= 245000,
-	.mpll_khz		= 245000
+	.mpll_khz		= 245000,
+	.max_vdd 		= TPS65023_MAX_DCDC1,
+	.acpu_set_vdd 		= qsd8x50_tps65023_set_dcdc1
 };
 
 static struct msm_acpu_clock_platform_data mahimahi_cdma_clock_data = {
@@ -978,7 +1007,9 @@ static struct msm_acpu_clock_platform_data mahimahi_cdma_clock_data = {
 	.vdd_switch_time_us	= 62,
 	.power_collapse_khz	= 235930,
 	.wait_for_irq_khz	= 235930,
-	.mpll_khz		= 235930
+	.mpll_khz		= 235930,
+	.max_vdd 		= TPS65023_MAX_DCDC1,
+	.acpu_set_vdd 		= qsd8x50_tps65023_set_dcdc1
 };
 
 static ssize_t mahimahi_virtual_keys_show(struct kobject *kobj,
