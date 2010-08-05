a6b933aa36f5db2daff30e9212324653c60ce32e
intersectR <rjgolo@gmail.com>
2010-08-05 22:52:42 +0800

    Revert "Add Hybrid Adaptive Voltage Scaling (HAVS)."
    This reverts commit 3c0ead6f0d800122259f88317269433a8c21272f.

--- include/linux/regulator/machine.h
+++ include/linux/regulator/machine.h
@@ -16,7 +16,6 @@
 #define __LINUX_REGULATOR_MACHINE_H_
 
 #include <linux/regulator/consumer.h>
-#include <linux/regulator/driver.h>
 #include <linux/suspend.h>
 
 struct regulator;
@@ -195,11 +194,5 @@ static inline void regulator_has_full_constraints(void)
 {
 }
 #endif
-
-extern int tps65023_dcdc_set_voltage(struct regulator_dev *dev,
-				int min_uV, int max_uV);
-
-extern int tps65023_set_dcdc1_level(struct regulator_dev *dev,
-				int mvolts);
 
 #endif
